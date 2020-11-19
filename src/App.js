903bdbed37cce8d836c974915b306c78b58c9be1
Sparsh Agarwal <sparshaga25@gmail.com>
2020-11-19 16:24:05 +0530

    Updated Card Design

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ function App() {
       img="https://image.tmdb.org/t/p/w600_and_h900_bestv2/or06FN3Dka5tukK1e9sl16pB3iy.jpg"
       title="Avengers: Endgame is Back for the spin"
       year="04/26/2019"
+      link="https://www.themoviedb.org/movie/299534-avengers-endgame"
     />
   );
 }
--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -4,8 +4,14 @@ import "../styles/MovieCard.css";
 function MovieCard(props) {
   return (
     <div className="card">
-      <img src={props.img} alt={props.title} className="poster" />
-      <h3 className="title">{props.title}</h3>
+      <a href={props.link} className="poster" title={props.title}>
+        <img src={props.img} alt={props.title} className="poster" />
+      </a>
+
+      <a className="title" href={props.link} title={props.title}>
+        {props.title}
+      </a>
+
       <h6 className="year">{props.year}</h6>
     </div>
   );
