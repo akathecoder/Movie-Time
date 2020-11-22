9b19d00425f7c702b5a4292884e01923237fb765
Sparsh Agarwal <sparshaga25@gmail.com>
2020-11-22 20:37:59 +0530

    Added Margins to HomePage

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -4,13 +4,29 @@ import "../styles/MovieCard.css";
 function MovieCard(props) {
   return (
     <div className="card">
-      <div href={props.link} className="poster-wrapper" title={props.title}>
-        <a href={props.link} className="poster-wrapper" title={props.title}>
-          <img src={props.img} alt={props.title} className="poster" />
+      <div
+        href={props.link}
+        className="poster-wrapper"
+        title={props.title}
+      >
+        <a
+          href={props.link}
+          className="poster-wrapper"
+          title={props.title}
+        >
+          <img
+            src={props.img}
+            alt={props.title}
+            className="poster"
+          />
         </a>
       </div>
 
-      <a className="title" href={props.link} title={props.title}>
+      <a
+        className="title"
+        href={props.link}
+        title={props.title}
+      >
         {props.title}
       </a>
 
